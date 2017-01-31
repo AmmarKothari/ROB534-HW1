1735a0262a8cc98cbbb7880d2c6b3eba69073e5b
amarsbars <ammar.kothari@gmail.com>
2017-01-30 21:20:42 -0800

    dynamic inflation

---
+++ AmmarCode/Astar_dynamic_inflated.m
@@ -0,0 +1,114 @@
+% A*
+clear all
+close all
+fdir = fileparts(which(mfilename));
+cd(fdir)
+addpath('../4D')
+addpath('../AmmarCode')
+addpath('../pq')
+cd('..')
+
+% Constants %
+map = read_map_for_dynamics('maze2.pgm');
+move_cost = 1;
+user_epsilon = 10;
+time_limit = 30;
+
+% Initialization %
+epsilon = user_epsilon;
+tic
+while toc < time_limit
+    t_start = tic;
+    goal = get_goal_dynamic(map);
+    [Xg, Yg, dx_g, dy_g] = dynamic_state_from_index(map,goal);
+    [start, num_nodes] = get_start_dynamic(map);
+    current_location = start;
+    maxV = map.maxV;
+    initial_cost = 20000;
+    cost_all = initial_cost*ones(num_nodes,1);
+    cost_all(start) = 0;
+    goal_flag = 0;
+    open_list = pq_init(1000);
+    open_list = pq_insert(open_list, start, cost_all(start));
+    iter = 0;
+    final_path = [];
+
+    while goal_flag ~= 1 && iter < 25*25*3*3*5
+        iter = iter + 1;
+        if current_location == goal
+            goal_flag = 1;
+            break
+        end
+    %     [Xc, Yc, dx_c, dy_c] = dynamic_state_from_index(map,current_location);
+    %     if [Xg-Xc,Yg-Yc] == [1,1]
+    %         goal_flag = 1;
+    %         break
+    %     end
+
+        try
+            [open_list, current_location] = pq_pop(open_list);
+        catch error
+            hello = 1;
+        end
+        [neighbors, num_neighbors] = get_neighbors_dynamic(map, current_location);
+        neighbors = neighbors((neighbors == current_location)~=1); %removing self from neighbors
+        current_cost = cost_all(current_location);
+        %if a location has no neighbors (a corner) then assign a high cost
+        if isempty(neighbors)
+            cost_all(current_location) = -initial_cost;
+        end
+        %last value of neighbors is the current location
+        %assign neighbors a new cost = move_cost + hueristic
+        for i = 1:length(neighbors)
+            neighbor_cost = current_cost + move_cost + Hueristic_Dynamic(neighbors(i), map);
+    %         [x, y, dx, dy] = dynamic_state_from_index(map, neighbors(i));
+    %         test_dynamic_neighbors(map, x, y, dx, dy)
+    %         pause(0.01)
+            if neighbor_cost < cost_all(neighbors(i))
+%                disp('Location Being Checked: ' + string(neighbors(i)) + ', Old Cost: ' + string(cost_all(neighbors(i))) + ', New Cost: ' + string(neighbor_cost))
+               cost_all(neighbors(i)) = neighbor_cost; 
+               open_list = pq_insert(open_list, neighbors(i), neighbor_cost);
+            else
+%                 disp('Location Being Checked: ' + string(neighbors(i)) + ' had no new neighbors')
+                continue
+            end
+        end
+
+
+    end
+    % need to find path from goal to start
+    current_location = start;
+    while all(current_location ~= goal)
+        [X, Y, dx, dy] = dynamic_state_from_index(map, current_location);
+        final_path = [final_path; X,Y];
+%         disp('dx: ' + string(dx) + ', dy: ' + string(dy))
+        neighbors = get_neighbors_dynamic(map, current_location);
+%         neighbors = get_neighbors_dynamic_rev(map, current_location);
+        neighbors = neighbors((neighbors == current_location)~=1); %removing self from neighbors
+        if isempty(neighbors)
+            hello = 1;
+        end
+        [next_val, next_ind] = min(abs(cost_all(neighbors)));
+        next_loc = neighbors(next_ind);
+        current_location = next_loc;
+%         plot_path(map, final_path, 'blah')
+%         pause(0.01)
+    end
+    path_length = length(final_path);
+    disp('Epsilon ' + string(epsilon))
+    disp('Path Length: ' + string(path_length))
+    disp('Nodes Expanded: ' + string(iter))
+    disp('Run Time: ' + string(toc))
+    disp('---------')
+    plot_path(map, final_path, 'A* with Euclidian Distance Hueristic')
+%     saveas(gcf, sprintf('name%d', epsilon), 'png')
+    epsilon = epsilon - 0.5*(epsilon - 1);
+    if epsilon == 1
+        break
+    end
+    if epsilon < 1.001
+        epsilon = 1;
+    end
+    plot_path(map, final_path, 'A* with ManhattanPlus Distance Hueristic')
+    pause(0.01)
+end
---
+++ AmmarCode/get_neighbors_dynamic_rev.m
@@ -0,0 +1,39 @@
+% Functions for getting neighbors in 4D dynamics problem
+% By Geoff Gordon and Geoff Hollinger, 2007
+% Modified by Ammar Kothari, 2017
+
+% Return the neighbors of a given state when moving from goal to start.
+% MAP is the map of the environment
+% (an array of 1s and 0s denoting occupied and full cells respectively).
+% STATE is a vector describing the current state [X Y DX DY].  NBRS has one
+% row per neighbor (i.e., up to 4 rows).  Each row is the same format as
+% STATE, and results from applying a feasible action.  If there are no
+% feasible actions from STATE, then NBRS == [].
+
+function [nbrs, num_nbrs] = get_neighbors_dynamic_rev(map, stateID)
+
+[x,y,dx,dy] = dynamic_state_from_index(map,stateID);
+
+acts = [0 -1; 1 0; 0 1; -1 0; 0 0];
+nbrs = [];
+num_nbrs = 0;
+
+for i = 1:size(acts,1)
+    ax = acts(i,1);
+    ay = acts(i,2);
+    %check if we're over the speed limit
+    if(dx+ax < -map.maxV || dy+ay < -map.maxV || dx+ax > 0 || dy+ay > 0)
+        continue;
+    end
+    %check if this path collides with an obstacle or leaves the map
+    if (~check_hit(map, x, y, dx+ax, dy+ay))
+        nbrs = [nbrs; dynamic_index_from_state(map,x+dx+ax,y+dy+ay,abs(dx+ax),abs(dy+ay))];
+        num_nbrs = num_nbrs + 1;
+    end
+end
+
+return
+
+
+
+
